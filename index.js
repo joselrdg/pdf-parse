c3a5a44baa1393fd036d5d1543c74753d17698a5
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-04-24 21:30:09 +0200

    pdfscra

--- index.js
+++ index.js
@@ -1,44 +1,114 @@
 // ('./pdfs/341g03-es.pdf');
 const fs = require('fs');
 const pdf = require('pdf-parse');
+const url = './pdfs/341g03-es.pdf'
+const pdfObjt = (url) => {
 
-let dataBuffer = fs.readFileSync('./pdfs/341g03-es.pdf');
+    let dataBuffer = fs.readFileSync(url);
 
-// PDF.getValue({"type":"image"});
+    pdf(dataBuffer).then(function (data) {
+        let arrText = []
+        const arrParse = data.text.split(/\n \n{1,}/)
+        arrParse.forEach((element, i) => {
+            element = element.replace(/\n/g, ' ')
+            if (!(/fci|FCI/g).test(arrParse[i])) {
+                arrText.push(element)
+            }
+        });
+        let arrText2 = [];
+        arrText.forEach((element, i) => {
+            if (!(/^FEDERATION/).test(arrText[i])) {
+                arrText2.push(element)
+            }
+        });
+        let arrText3 = [];
+        arrText2.forEach((element, i) => {
+            element = element.replace(/ {1,}/g, ' ')
+            if (!(/[0-9]| /).test(arrText2[i][0])) {
+                element = element.replace(/ {1,}$/, '')
+                arrText3.push(element)
+            }
+        });
 
-pdf(dataBuffer).then(function (data) {
-    let arrText = []
-    const arrParse = data.text.split(/\n \n{1,}/)
-    arrParse.forEach((element, i) => {
-        let p = element
-            .replace(/(_{3,} | {10,}|\n)/g, '')
-            .replace((/^ | $/), '')
-            .replace(/ $/, '')
-        if (p !== '') { arrText.push(p) }
-    });
-    const arrText2 = []
-    arrText.forEach((element, i) => {
-        let p = element.split(/[A-Z]: /)
-        arrText2.push(p)
-    });
-    let cont1 = []
-    arrText2.forEach((e, i) => {
-        if (arrText2[i].length === 1) {
-            cont1.push(i)
+        let dataObj = {}
+
+        if ((/[()]/).test(arrText3[0])) {
+            let arr1 = arrText3[0].replace(')', '')
+            let arr = arr1.split('(')
+            dataObj.name = arr
+        } else {
+            dataObj.name = arrText3[0]
         }
-    })
-    const arrText3 = []
-    cont1.forEach((e, i) => {
-        arrText3.push(arrText2[cont1[i]])
-        // if (i === cont1[i]) {
-        //     arrText3.push(arrText2[i])
-        //     let cont = cont1[i]
-        //     do {
-        //         arrText3[i].push(arrText2[cont+1])
-        //         cont++
-        //     }while  (cont < cont1[i + 1] && cont < arrText2.length)
-        // }
-    })
+        const regx = [
+            (/^ORIGEN|^Origen/),
+            (/^FECHA|^Fecha/),
+            (/^UTILIZACI|^Utilizaci/),
+            (/^BREVE|^Breve/),
+            (/^APARIENCIA|^Apariencia/),
+            (/^COMPORTAMIENTO|^Comportamiento/),
+            (/^CABEZA|^Cabeza/),
+            (/^REGION CRANEAL|^Region craneal/),
+            (/^REGION FACIAL|^Region facial/),
+            (/^OJOS|^Ojos/),
+            (/^OREJAS|^Orejas/),
+            (/^CUELLO|^Cuello/),
+            (/^CUERPO|^Cuerpo/),
+            (/^MIEMBROS ANTERIORES|^Miembros anteriores/),
+            (/^MIEMBROS POSTERIORES|^Miembros posteriores/),
+            (/^MOVIMIENTO|^Movimiento/),
+            (/^PIEL|^Piel/),
+            (/^PELO|^Pelo/),
+            (/^COLOR|^Color/),
+            (/^TAMAÑO|^Tamaño/),
+            (/^FALTAS|^Faltas/),
+            (/^FALTAS GRAVES|^Faltas graves/),
+            (/^FALTAS DESCALIFICANTES|^Faltas descalificantes/),
+        ]
+        const nom = [
+            'origen',
+            'fecha',
+            'utilizado',
+            'historia',
+            'apariencia',
+            'comportamiento',
+            'cabeza',
+            'craneal',
+            'facial',
+            'ojos',
+            'orejas',
+            'cuello',
+            'cuerpo',
+            'eanteriores',
+            'eposteriores',
+            'movimiento',
+            'piel',
+            'pelo',
+            'color',
+            'tamano',
+            'faltas',
+            'graves',
+            'descalifivantes',
+        ]
+        arrText3.forEach((element, i) => {
+            element = element.replace(/(.)$/, '')
+            regx.forEach((e, ind) => {
+                if ((e).test(element)) {
+                    if ((/: /g).test(element)) {
+                        let arr = element.split(/: /)
+                        if (arr.length === 2) {
+                            dataObj[nom[ind]] = arr[1]
+                        } else {
+                            dataObj[nom[ind]] = arr
+                        }
+                    } else {
+                        dataObj[nom[ind]] = element // optimizar
+                    }
+                }
+            });
+        });
+        console.log(arrText3.length)
+        console.log(dataObj)
+    });
+}
 
-    console.log(arrText2)
-});
+pdfObjt(url)
