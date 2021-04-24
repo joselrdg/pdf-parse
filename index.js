4bc3aa39521be8a71cd1d2695e1e134ece1f322c
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-04-24 02:49:49 +0200

    arr

--- index.js
+++ index.js
@@ -7,11 +7,38 @@ let dataBuffer = fs.readFileSync('./pdfs/341g03-es.pdf');
 // PDF.getValue({"type":"image"});
 
 pdf(dataBuffer).then(function (data) {
-    let pdf = {}
-    // console.log(data);
-    console.log(data.text.length);
-    let star = 0
-    let arrParse = data.text.split(/\n \n{1,}/)
-    console.log(arrParse)
+    let arrText = []
+    const arrParse = data.text.split(/\n \n{1,}/)
+    arrParse.forEach((element, i) => {
+        let p = element
+            .replace(/(_{3,} | {10,}|\n)/g, '')
+            .replace((/^ | $/), '')
+            .replace(/ $/, '')
+        if (p !== '') { arrText.push(p) }
+    });
+    const arrText2 = []
+    arrText.forEach((element, i) => {
+        let p = element.split(/[A-Z]: /)
+        arrText2.push(p)
+    });
+    let cont1 = []
+    arrText2.forEach((e, i) => {
+        if (arrText2[i].length === 1) {
+            cont1.push(i)
+        }
+    })
+    const arrText3 = []
+    cont1.forEach((e, i) => {
+        arrText3.push(arrText2[cont1[i]])
+        // if (i === cont1[i]) {
+        //     arrText3.push(arrText2[i])
+        //     let cont = cont1[i]
+        //     do {
+        //         arrText3[i].push(arrText2[cont+1])
+        //         cont++
+        //     }while  (cont < cont1[i + 1] && cont < arrText2.length)
+        // }
+    })
 
+    console.log(arrText2)
 });
