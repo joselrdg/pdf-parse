7fa49cc3cfdc828dc094f8f7722636e440b297e4
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-04-25 10:26:53 +0200

    fix nombre

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
 // ('./pdfs/341g03-es.pdf');
 const fs = require('fs');
 const pdf = require('pdf-parse');
-const url = './pdfs/341g03-es.pdf'
-const pdfObjt = (url) => {
+// const url = './pdfs/341g03-es.pdf'
+const arrayUrlS = []
+const arrData = []
+
+// // grupo1
+const numeroPdfs = 42;
+// // grupo2
+// const numeroPdfs = 52;
+// // grupo3
+// const numeroPdfs = 34;
+// // grupo4 = 0
+// // grupo5
+// const numeroPdfs = 40;
+// // grupo6
+// const numeroPdfs = 69;
+// // grupo7
+// const numeroPdfs = 34;
+// // grupo8
+// const numeroPdfs = 22;
+// // grupo9
+// const numeroPdfs = 26;
+// // grupo10
+// const numeroPdfs = 13;
+
+const nombPdf = []
+
+for (let i = 0; i < numeroPdfs; i++) {
+    const name = `./pdfs/grupo1/${i}.pdf`
+    nombPdf.push(name)
+}
+
+console.log(nombPdf)
+
+let contador = 0
+
+
+const contadorFun = () => {
+    if (contador < numeroPdfs) {
+        resolveAfter(nombPdf[contador])
+    } else {
+        console.log(arrData)
+        return console.log('Tarea terminada totalmente')
+    }
+}
+
+async function resolveAfter(url) {
 
     let dataBuffer = fs.readFileSync(url);
 
@@ -12,7 +56,9 @@ const pdfObjt = (url) => {
         arrParse.forEach((element, i) => {
             element = element.replace(/\n/g, ' ')
             if (!(/fci|FCI/g).test(arrParse[i])) {
-                arrText.push(element)
+                if (!((/^TRADUCCI|^Traducci/)).test(element)) {
+                    arrText.push(element)
+                }
             }
         });
         let arrText2 = [];
@@ -24,20 +70,29 @@ const pdfObjt = (url) => {
         let arrText3 = [];
         arrText2.forEach((element, i) => {
             element = element.replace(/ {1,}/g, ' ')
-            if (!(/[0-9]| /).test(arrText2[i][0])) {
+            element = element.replace(/^ |^  /, '')
+            if (!(/^[0-9]/).test(element)) {
                 element = element.replace(/ {1,}$/, '')
                 arrText3.push(element)
+
             }
         });
 
+        let arrText4 = []
+        arrText3.forEach((e => {
+            if (!(/^[0-9]/).test(e) && e.length > 2) {
+                arrText4.push(e)
+            }
+        }))
+
         let dataObj = {}
 
-        if ((/[()]/).test(arrText3[0])) {
-            let arr1 = arrText3[0].replace(')', '')
+        if ((/[()]/).test(arrText4[0])) {
+            let arr1 = arrText4[0].replace(')', '')
             let arr = arr1.split('(')
             dataObj.name = arr
         } else {
-            dataObj.name = arrText3[0]
+            dataObj.name = arrText4[0]
         }
         const regx = [
             (/^ORIGEN|^Origen/),
@@ -89,7 +144,7 @@ const pdfObjt = (url) => {
             'graves',
             'descalifivantes',
         ]
-        arrText3.forEach((element, i) => {
+        arrText4.forEach((element, i) => {
             element = element.replace(/(.)$/, '')
             regx.forEach((e, ind) => {
                 if ((e).test(element)) {
@@ -106,9 +161,20 @@ const pdfObjt = (url) => {
                 }
             });
         });
-        console.log(arrText3.length)
-        console.log(dataObj)
+        // dataObj.text = arrText4
+        arrData.push(dataObj)
+        console.log('trabajo hecho')
+        contador++
+        contadorFun()
     });
 }
 
-pdfObjt(url)
+// resolveAfter('./pdfs/grupo1/41.pdf')
+contadorFun()
+
+// for (urls in arrayUrlS) {
+//    const data = await resolveAfter(arrayUrlS[urls])
+//    arrData.push(data)
+//     console.log('trabajo hecho')
+// }
+
